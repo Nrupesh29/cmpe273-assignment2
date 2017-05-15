401f7e908e3b2c6b572b5d5ae9d282b137693aac
Nrupesh Patel <nrupesh.patel2912@gmail.com>
2017-05-15 11:05:54 -0700

    Added Create OrderFunction

---
+++ Order/CreateOrder/service.py
@@ -0,0 +1,22 @@
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+def lambda_handler(event, context):
+
+    menu_id = event['menu_id']
+
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('Menus')
+    
+    event.update({"sequence": [
+        "selection",
+        "size"
+    ]})
+
+    table.put_item(
+        Item={
+            'menuId': menu_id,
+            'payload': event
+        }
+    )
